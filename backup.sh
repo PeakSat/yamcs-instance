1ed8778fa198dd84642497237951927d02838b32
Stavrenas <stavrina25@gmail.com>
2022-08-22 18:31:47 +0300

    add restore command and ensure it works

--- backup.sh
+++ backup.sh
@@ -6,8 +6,9 @@
 # The yamcsadmin script is in the directory target/bundle-tmp/bin/yamcsadmin. 
 
 # In order to:
-# - Create a new hot backup at the backups directory* : " ./target/bundle-tmp/bin/yamcsadmin backup create --backup-dir ../../backups --host localhost:9999 myproject " (works but does not display a confirmation message)
-# - Create a new cold backup (instance not running) at the backups directory: " ./target/bundle-tmp/bin/yamcsadmin backup create --backup-dir ../../backups --data-dir {YAMCS_FOLDER}/target/yamcs/yamcs-data myproject "
+# - Create a new hot backup at the backups directory* : " ./target/bundle-tmp/bin/yamcsadmin backup create --backup-dir {BACKUP-PATH} --host localhost:9999 myproject " (works but does not display a confirmation message).
+# - Create a new cold backup (instance not running) at the backups directory: " ./target/bundle-tmp/bin/yamcsadmin backup create --backup-dir {BACKUP-PATH} --data-dir {YAMCS_FOLDER}/target/yamcs/yamcs-data myproject ".
+# - Restore a backup: " ./target/bundle-tmp/bin/yamcsadmin backup restore --backup-dir {BACKUP-PATH} --restore-dir ../../target/yamcs/yamcs-data/myproject.rdb {ID} ", where ID is the id of the backup you want to restore. 
 
 # Make sure you start yamcs using the command: 
 # mvn yamcs:run -Dyamcs.jvmArgs="-Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
