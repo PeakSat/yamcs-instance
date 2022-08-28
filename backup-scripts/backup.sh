f67d4cd9b19dd77681d723311e0b6a5555c54ea9
Stavrenas <stavrina25@gmail.com>
2022-08-28 03:23:20 +0300

    fix spelling and add note

--- backup-scripts/backup.sh
+++ backup-scripts/backup.sh
@@ -3,7 +3,7 @@
 # Time interval in seconds
 time=30s
 
-# Backup path (the root directory is /target/yamcs)
+# Backup path (the root directory is /target/yamcs). Change also the ./gdrive argument if this is altered. 
 path=../../backups
 
 # The Google Drive folder 
@@ -13,10 +13,10 @@ folder_id=1MQDNgPfBtXBGCVxOcK3GrmajePFCZewy
 ./gdrive about
 
 while [ : ]; do
-    #  this is a hot backup, meaning yamcs-instance is still running
+    # this is a hot backup, meaning yamcs-instance is still running
     ./../target/bundle-tmp/bin/yamcsadmin backup create --backup-dir $path --host localhost:9999 myproject
     echo "Backup complete. List backups with the command ./../target/bundle-tmp/bin/yamcsadmin backup list --backup-dir $path"
-    # this is the gdrive upload commmand that mirrors the backups folder to a specific Google Drive folder (currently this is a folder in an accoubnt dedicated for backups yamcs.backup.acubesat@gmail.com).
+    # this is the gdrive upload commmand that mirrors the backups folder to a specific Google Drive folder (currently this is a folder in an account dedicated for backups: yamcs.backup.acubesat@gmail.com).
     # if you want to try it yourself , create a new folder on your personal drive and copy the id 
      ./gdrive sync upload ../backups $folder_id
     sleep $time
