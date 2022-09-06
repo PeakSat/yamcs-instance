3bec825359a1eb079c85e6ad476a6416b47ce615
Nantina <nbosinaki@gmail.com>
2022-09-06 17:33:51 +0300

    UDP-TCP simulators

--- simulatorudp.py
+++ simulatorudp.py
@@ -9,73 +9,29 @@ from time import sleep
 
 
 def send_tm(simulator):
-    host10015='localhost'
-    port10015=10015
-    tm_socket_10015 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    host10016='localhost'
-    port10016=10016
-    tm_socket_10016 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    host10017='localhost'
-    port10017=10017
-    tm_socket_10017 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    tm_socket_10015.bind((host10015,port10015))
-    tm_socket_10015.listen(1)
-    print ('\n','server  10015 1istening')
-
-    tm_socket_10016.bind((host10016,port10016))
-    tm_socket_10016.listen(1)
-    print ('\n','server  10016 1istening')
-
-    tm_socket_10017.bind((host10017,port10017))
-    tm_socket_10017.listen(1)
-    print ('\n','server  10017 1istening')
-
-    clientconn10015, clientaddr10015=tm_socket_10015.accept()
-    print ('\n','connection 10015 established with',clientaddr10015)
-
-    clientconn10016, clientaddr10016=tm_socket_10016.accept()
-    print ('\n','connection 10016 established with',clientaddr10016)
-
-    clientconn10017, clientaddr10017=tm_socket_10017.accept()
-    print ('\n','connection 10017 established with',clientaddr10017)
-
+    tm_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     n=0
-    simulator.tm_counter = 0
-    
-#sending 4500 packets
-    while n<900:
-        with io.open('packets-1.raw', 'rb') as f:
-            header = bytearray(6)
 
+#sending approximately 1800 packets
+    while n<2:
+
+        with io.open('packets.raw', 'rb') as f:
+            simulator.tm_counter = 1
+            header = bytearray(6)
             while f.readinto(header) == 6:
                 (len,) = unpack_from('>H', header, 4)
-                packet = bytearray(len+7)
 
+                packet = bytearray(len+6)
                 f.seek(-6, io.SEEK_CUR)
                 f.readinto(packet)
-    
-                clientconn10015.send(packet)
-                print('\n', 'packet sent 10015')
 
-                clientconn10016.send(packet)
-                print('\n', 'packet sent 10016')
-
-                clientconn10017.send(packet)
-                print('\n', 'packet sent 10017')
-                simulator.tm_counter+=1
-
+                tm_socket.sendto(packet, ('127.0.0.1', 10015))
+                tm_socket.sendto(packet, ('127.0.0.1', 10016))
+                tm_socket.sendto(packet, ('127.0.0.1', 10017))
+                simulator.tm_counter += 1
 
                 sleep(0.01)
         n+=1
-            
-
-    clientconn10015.close()
-    clientconn10016.close()
-    clientconn10017.close()
-    print("communication ended")
 
 
 def receive_tc(simulator):
