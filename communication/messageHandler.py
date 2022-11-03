e229c56837b5016505e3252594ee42e33bc6cbad
Stavrenas <stavrina25@gmail.com>
2022-11-04 01:25:18 +0200

    inital attempt to implement async network io

--- communication/messageHandler.py
+++ communication/messageHandler.py
@@ -6,6 +6,7 @@ from threading import Thread
 from dataclasses import dataclass
 from logging import config
 import logging
+import asyncio
 import socket
 from time import sleep
 import yaml
@@ -15,6 +16,7 @@ from cobs import cobs
 EXCLAMATION_MARK = 0x021
 SPACE = 0x020
 DELIMITER = b"\x00"
+loop = asyncio.new_event_loop
 
 
 class YAMCSClosedPortException(Exception):
@@ -94,45 +96,25 @@ def connect_to_port(settings: Settings, port: int) -> socket:
         yamcs_socket.bind((settings.IPv4, port))
     except OSError:
         logging.exception(
-            "Port "
-            + str(port)
-            + " not available (check if YAMCS is running)."
+            "Port " + str(port) + " not available (check if YAMCS is running)."
         )
         exit(1)
 
     yamcs_socket.listen(settings.socket_backlog_level)
-    yamcs_client, _ = yamcs_socket.accept()
+    # yamcs_client, _ = yamcs_socket.accept()
     logging.debug("Connected to " + str(port))
-    return yamcs_client
+    return yamcs_socket
 
 
-def mcu_client(settings: Settings, serial_port: str = None, yamcs_port_in: int = None):
+async def connect_to_port_background(settings: Settings, port: int, serial_port: str):
     """
-
-    Opens a new TCP stream socket.
-    It listens to the specified serial port for TM messages.
-    They usually come in the following form:
-    '1801 [debug    ]OBC New TM[3,25] message! 8 1 192 2 0 15 32 3 25 0 2 0 1 37 165 53 0 0 0 0 0 \n'
-    This string is split after the "!", returning the actual packet.
-    All the bytes after that are sent to YAMCS.
-    If we try to convert the characters one by one from ASCII to integer, we will get something like:
-    8 1 1 9 2 0 0 2 0 ... -> garbage
-    So we need to parse a sequence of numbers as a single decimal.
-    In order to do this, we must first convert all ascii numbers to decimal form.
-    Also we need to keep track of the space characters. If we receive numbers one after the other
-    (whithout space characters in between), we need to multiply the previous entry by 10, in order
-    to parse the whole decimal.
-    Note:
-        If the debugging messages are altered, this script will have undetermined behavior, since
-        it relies on the existence of the exclamation mark "!" to detect actual TMs being sent.
+    This function is used to connect to a TCP socket.
+    If the processes initialized by this script are not terminated properly (this can happen
+    if an exception occurs),the TCP connections might not close. By enabling the REUSEADDR
+    (reuse address) option, the script will try to reconnect to the already opened port
+    that is in a TIME_WAIT state.
     """
-    if yamcs_port_in is None:
-        yamcs_port_in = settings.obc_port_in
-    if serial_port is None:
-        serial_port = settings.usb_serial_0
-
-    tcp_client = connect_to_port(settings, yamcs_port_in)
-
+    _, writer = await asyncio.open_connection(settings.IPv4, port)
     while True:
 
         try:
@@ -166,7 +148,8 @@ def mcu_client(settings: Settings, serial_port: str = None, yamcs_port_in: int =
                         packet_byte_int = packet_byte - 48
                         packet_byte_decimal = packet_byte_decimal * 10 + packet_byte_int
 
-                tcp_client.send(bytes(packet))
+                writer.write(bytes(packet))
+                await writer.drain()
         except serial.SerialException:
             logging.warning(
                 "No device is connected at port "
@@ -174,8 +157,34 @@ def mcu_client(settings: Settings, serial_port: str = None, yamcs_port_in: int =
                 + ". Please connect a device."
             )
             sleep(settings.reconnection_timeout)
-        except socket.error as error:
-            raise YAMCSClosedPortException(error)
+
+
+def mcu_client(settings: Settings, serial_port: str = None, yamcs_port_in: int = None):
+    """
+
+    Opens a new TCP stream socket.
+    It listens to the specified serial port for TM messages.
+    They usually come in the following form:
+    '1801 [debug    ]OBC New TM[3,25] message! 8 1 192 2 0 15 32 3 25 0 2 0 1 37 165 53 0 0 0 0 0 \n'
+    This string is split after the "!", returning the actual packet.
+    All the bytes after that are sent to YAMCS.
+    If we try to convert the characters one by one from ASCII to integer, we will get something like:
+    8 1 1 9 2 0 0 2 0 ... -> garbage
+    So we need to parse a sequence of numbers as a single decimal.
+    In order to do this, we must first convert all ascii numbers to decimal form.
+    Also we need to keep track of the space characters. If we receive numbers one after the other
+    (whithout space characters in between), we need to multiply the previous entry by 10, in order
+    to parse the whole decimal.
+    Note:
+        If the debugging messages are altered, this script will have undetermined behavior, since
+        it relies on the existence of the exclamation mark "!" to detect actual TMs being sent.
+    """
+    if yamcs_port_in is None:
+        yamcs_port_in = settings.obc_port_in
+    if serial_port is None:
+        serial_port = settings.usb_serial_0
+
+    asyncio.run(connect_to_port_background(settings, yamcs_port_in, serial_port))
 
 
 def yamcs_client(settings: Settings, serial_port: str = None):
@@ -189,7 +198,8 @@ def yamcs_client(settings: Settings, serial_port: str = None):
     if serial_port is None:
         serial_port = settings.usb_serial_0
 
-    tcp_client = connect_to_port(settings, settings.yamcs_port_out)
+    tcp_socket = connect_to_port(settings, settings.yamcs_port_out)
+    tcp_client, _ = tcp_socket.accept()
 
     while True:
 
@@ -262,20 +272,20 @@ if __name__ == "__main__":
     )
     obc_listener_thread.start()
 
-    adcs_listener_thread = Thread(
-        target=mcu_client,
-        args=(
-            settings,
-            adcs_serial_port,
-            settings.adcs_port_in,
-        ),
-    ).start()
+    # adcs_listener_thread = Thread(
+    #     target=mcu_client,
+    #     args=(
+    #         settings,
+    #         adcs_serial_port,
+    #         settings.adcs_port_in,
+    #     ),
+    # ).start()
 
-    can_listener_thread = Thread(
-        target=mcu_client,
-        args=(
-            settings,
-            can_serial_port,
-            settings.canBus_port_in,
-        ),
-    ).start()
+    # can_listener_thread = Thread(
+    #     target=mcu_client,
+    #     args=(
+    #         settings,
+    #         can_serial_port,
+    #         settings.canBus_port_in,
+    #     ),
+    # ).start()
