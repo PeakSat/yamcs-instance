f99f58636eafa9a08cb885464ff46dac6353bc24
Stavrenas <stavrina25@gmail.com>
2022-11-29 23:22:31 +0200

    create first class and add sample image

---
+++ src/main/java/gr/spacedot/acubesat/FileHandling/FileEntity.java
@@ -0,0 +1,8 @@
+package gr.spacedot.acubesat.FileHandling;
+
+public class FileEntity {
+    private String path;
+
+    private String name;
+
+}
---
+++ src/main/java/gr/spacedot/acubesat/FileHandling/FileSender.java
@@ -0,0 +1,10 @@
+package gr.spacedot.acubesat.FileHandling;
+
+class FileSender {
+
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hey bitch");
+    }
+}
