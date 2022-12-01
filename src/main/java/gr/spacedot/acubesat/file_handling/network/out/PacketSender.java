6b893cb2ee538495b1c64f150d2ef735d464c383
Stavrenas <stavrina25@gmail.com>
2022-12-01 21:45:29 +0200

    send file using string instead of byte array

--- src/main/java/gr/spacedot/acubesat/file_handling/network/out/PacketSender.java
+++ src/main/java/gr/spacedot/acubesat/file_handling/network/out/PacketSender.java
@@ -3,6 +3,7 @@ package gr.spacedot.acubesat.file_handling.network.out;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -21,6 +22,7 @@ public class PacketSender {
         HashMap<String, String> stringArguments = new HashMap<>();
         stringArguments.put("target_file_path", chunkedFileEntity.getPath());
         stringArguments.put("target_file_name", chunkedFileEntity.getName());
+        stringArguments.put("data", null);
 
         List<byte[]> chunks = chunkedFileEntity.getChunks();
         int totalChunks = chunks.size();
@@ -30,32 +32,32 @@ public class PacketSender {
         integerArguments.put("total_chunks", totalChunks);
         integerArguments.put("chunk_size", null);
 
-        HashMap<String, byte[]> byteArrayArguments = new HashMap<>();
-        byteArrayArguments.put("data", null);
 
         try {
 
             for (int chunk = 0; chunk < totalChunks; chunk++) {
 
                 byte[] currentChunk = chunks.get(chunk);
+                String data = new String(currentChunk, "UTF-8");
 
                 integerArguments.replace("current_chunk", chunk);
                 integerArguments.replace("chunk_size", currentChunk.length);
 
-                byteArrayArguments.replace("data", currentChunk);
+                // stringArguments.replace("data", data);
 
                 String stringBody = objectMapper.writeValueAsString(stringArguments);
                 String integerBody = objectMapper.writeValueAsString(integerArguments);
-                String byteBody = objectMapper.writeValueAsString(byteArrayArguments);
 
-                String requestBody = stringBody + integerBody + byteBody;
+                String argumentsBody = stringBody + integerBody;
+                HashMap<String,String> args = new HashMap<>();
+                args.put("args",argumentsBody);
 
                 HttpRequest request = HttpRequest.newBuilder()
                         .uri(URI.create(
                                 "http://localhost:8090/api/processors/AcubeSAT/realtime/commands/file-handling/TC(24,1):send_file_segment"))
-                        .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                        .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(args)))
                         .build();
-                System.out.println("REQUEST SENT");
+                System.out.println("REQUEST SENT " + args);
 
                 try {
                     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -66,6 +68,8 @@ public class PacketSender {
             }
         } catch (JsonProcessingException e) {
             System.out.println("Error processing json: " + e);
+        } catch (UnsupportedEncodingException e1) {
+            System.out.println("Byte array to string conversion failed with error : " + e1);
         }
     }
 
