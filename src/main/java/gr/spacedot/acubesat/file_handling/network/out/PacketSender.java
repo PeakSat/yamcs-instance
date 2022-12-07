6ca2053f741cada0c4d99de211863287c4aeb123
Stavrenas <stavrina25@gmail.com>
2022-12-07 17:12:27 +0200

    convert byte array to hex string

--- src/main/java/gr/spacedot/acubesat/file_handling/network/out/PacketSender.java
+++ src/main/java/gr/spacedot/acubesat/file_handling/network/out/PacketSender.java
@@ -5,20 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Base64;
+import java.util.ArrayList;
+import javax.xml.bind.DatatypeConverter;
 import java.util.List;
 import java.util.logging.Logger;
 
 import com.google.common.primitives.Bytes;
 import com.google.gson.JsonObject;
 import gr.spacedot.acubesat.file_handling.entities.ChunkedFileEntity;
-import io.netty.handler.codec.spdy.SpdyHttpResponseStreamIdHandler;
 
 
 public class PacketSender {
     private static final Logger LOGGER = Logger.getLogger(PacketSender.class.getName());
 
-    public static int THRESHOLD_BYTES = 64000;
+    public static final int THRESHOLD_BYTES = 64000;
 
     /**
      * It sends each file chunk in a separate TC, including all important
@@ -33,49 +33,78 @@ public class PacketSender {
         HttpClient client = HttpClient.newHttpClient();
 
         List<byte[]> chunks = chunkedFileEntity.getChunks();
-        int totalChunks = chunks.size();
 
+        int offset = 0;
 
-        JsonObject mainBody = new JsonObject();
+        while (offset != -1) {
+            JsonObject mainBody = new JsonObject();
+            mainBody.addProperty("base", new File(chunkedFileEntity.getPath(), chunkedFileEntity.getName()).toString());
+            offset = putChunksIntoPackets(chunks, mainBody, offset);
 
-        byte[] currentChunk = chunks.get(chunk);
-        String data = Base64.getEncoder().encodeToString(currentChunk);
+            JsonObject args = new JsonObject();
+            args.add("args", mainBody);
 
-        mainBody.addProperty("base", new File(chunkedFileEntity.getPath(), chunkedFileEntity.getName()).toString());
-        mainBody.addProperty("number_of_objects", totalChunks);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(
+                            "http://localhost:8090/api/processors/AcubeSAT/realtime/commands/file-handling/TC(6,1)_load_object_memory_data"))
+                    .POST(HttpRequest.BodyPublishers.ofString(args.toString()))
+                    .build();
 
-        mainBody.addProperty("binary_data", dataToLoad);
-
-        JsonObject args = new JsonObject();
-        args.add("args", mainBody);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(
-                        "http://localhost:8090/api/processors/AcubeSAT/realtime/commands/file-handling/TC(6,1)_load_object_memory_data"))
-                .POST(HttpRequest.BodyPublishers.ofString(args.toString()))
-                .build();
-
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            try {
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 //                LOGGER.info(response.body());
-        } catch (Exception e) {
-            LOGGER.info("Error sending request " + e);
+            } catch (Exception e) {
+                LOGGER.info("Error sending request " + e);
+            }
         }
 
 
     }
 
-    private byte[] getDataSegmentForPacket(List<byte[]> chunks, int initialOffset) {
+    /**
+     * Places the chunks into groups smaller than the maximum CCSDS packet limit.
+     *
+     * @param chunks: The chunks of the file to be transmitted
+     * @return the starting index for the next packet and -1 If all chunks have been sent.
+     */
+    private int putChunksIntoPackets(List<byte[]> chunks, JsonObject mainbody, int start) {
+
+        if (start == chunks.size() - 1)
+            return -1;
         int messageSize = 0;
-        int offset = initialOffset;
-        int currentChunk;
+        int offset = start;
+        int lastChunkIndex = 0;
 
-        while (messageSize < THRESHOLD_BYTES) {
-            byte[] information = {(byte) offset, (byte) currentChunk.length};
-            byte[] dataToLoad = Bytes.concat(information);
+        byte[] currentPacket = {};
+        List<byte[]> packetData = new ArrayList<>();
 
+        for (int chunk = offset; chunk < chunks.size(); chunk++) {
+            byte[] currentChunk = chunks.get(chunk);
+            int chunkLength = currentChunk.length;
+
+            byte[] information = {(byte) offset, (byte) chunkLength};
+            offset += chunkLength;
+            messageSize += chunkLength + information.length;
+
+            if (messageSize > THRESHOLD_BYTES) {
+                packetData.add(currentPacket);
+                lastChunkIndex = chunk;
+                break;
+            } else {
+                System.out.println("Added chunk "+chunk);
+                currentPacket = Bytes.concat(currentPacket, information, currentChunk);
+            }
         }
+        mainbody.addProperty("number_of_objects", lastChunkIndex + 1);
+        System.out.println("number_of_objects is "+lastChunkIndex + 1);
 
+        byte[] finalPacket = {};
+        for (byte[] chunk : packetData) {
+            finalPacket = Bytes.concat(finalPacket, chunk);
+        }
+        mainbody.addProperty("binary_data", DatatypeConverter.printHexBinary(finalPacket));
+
+        return offset;
     }
 
 }
