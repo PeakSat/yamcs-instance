a518e98788ec797a985b8e62a5a9e454154f073b
Stavrenas <stavrina25@gmail.com>
2022-12-07 13:41:39 +0200

    refactor PacketSender

--- src/main/java/gr/spacedot/acubesat/file_handling/network/out/PacketSender.java
+++ src/main/java/gr/spacedot/acubesat/file_handling/network/out/PacketSender.java
@@ -9,12 +9,16 @@ import java.util.Base64;
 import java.util.List;
 import java.util.logging.Logger;
 
+import com.google.common.primitives.Bytes;
 import com.google.gson.JsonObject;
 import gr.spacedot.acubesat.file_handling.entities.ChunkedFileEntity;
+import io.netty.handler.codec.spdy.SpdyHttpResponseStreamIdHandler;
 
 
 public class PacketSender {
     private static final Logger LOGGER = Logger.getLogger(PacketSender.class.getName());
+
+    public static int THRESHOLD_BYTES = 64000;
 
     /**
      * It sends each file chunk in a separate TC, including all important
@@ -23,7 +27,6 @@ public class PacketSender {
      * TODO: Log response only if it is not of status 200: OK
      *
      * @param chunkedFileEntity : the already split file to be sent.
-     *
      */
     public void sentPacketSegments(ChunkedFileEntity chunkedFileEntity) {
 
@@ -32,36 +35,47 @@ public class PacketSender {
         List<byte[]> chunks = chunkedFileEntity.getChunks();
         int totalChunks = chunks.size();
 
-        for (int chunk = 0; chunk < totalChunks; chunk++) {
 
-            JsonObject mainBody = new JsonObject();
+        JsonObject mainBody = new JsonObject();
 
-            byte[] currentChunk = chunks.get(chunk);
-            String data = Base64.getEncoder().encodeToString(currentChunk);
+        byte[] currentChunk = chunks.get(chunk);
+        String data = Base64.getEncoder().encodeToString(currentChunk);
 
-            mainBody.addProperty("base", new File(chunkedFileEntity.getPath(),chunkedFileEntity.getName()).toString());
-            mainBody.addProperty("total_chunks", totalChunks);
-            mainBody.addProperty("current_chunk", chunk);
-            mainBody.addProperty("chunk_size", data.length() * 8); //get bits
-            mainBody.addProperty("data", data);
+        mainBody.addProperty("base", new File(chunkedFileEntity.getPath(), chunkedFileEntity.getName()).toString());
+        mainBody.addProperty("number_of_objects", totalChunks);
 
-            JsonObject args = new JsonObject();
-            args.add("args", mainBody);
+        mainBody.addProperty("binary_data", dataToLoad);
 
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(
-                            "http://localhost:8090/api/processors/AcubeSAT/realtime/commands/file-handling/TC(24,1):send_file_segment"))
-                    .POST(HttpRequest.BodyPublishers.ofString(args.toString()))
-                    .build();
+        JsonObject args = new JsonObject();
+        args.add("args", mainBody);
 
-            try {
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(
+                        "http://localhost:8090/api/processors/AcubeSAT/realtime/commands/file-handling/TC(6,1)_load_object_memory_data"))
+                .POST(HttpRequest.BodyPublishers.ofString(args.toString()))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 //                LOGGER.info(response.body());
-            } catch (Exception e) {
-                LOGGER.info("Error sending request " + e);
-            }
+        } catch (Exception e) {
+            LOGGER.info("Error sending request " + e);
+        }
+
+
+    }
+
+    private byte[] getDataSegmentForPacket(List<byte[]> chunks, int initialOffset) {
+        int messageSize = 0;
+        int offset = initialOffset;
+        int currentChunk;
+
+        while (messageSize < THRESHOLD_BYTES) {
+            byte[] information = {(byte) offset, (byte) currentChunk.length};
+            byte[] dataToLoad = Bytes.concat(information);
 
         }
+
     }
 
 }
