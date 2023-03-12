4fa03442bb864af329390fe7a6405f37228744c3
NikolasTsm <ntsamitr@ece.auth.gr>
2023-03-13 00:43:43 +0200

    upload files to bucket

--- src/main/java/gr/spacedot/acubesat/file_handling/network/in/TMParser.java
+++ src/main/java/gr/spacedot/acubesat/file_handling/network/in/TMParser.java
@@ -3,13 +3,23 @@ package gr.spacedot.acubesat.file_handling.network.in;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.net.URI;
 import java.nio.ByteBuffer;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.logging.Logger;
+
+import com.google.gson.JsonObject;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
 
 import gr.spacedot.acubesat.file_handling.entities.FileEntity;
 import gr.spacedot.acubesat.file_handling.enums.LocalPaths;
 import gr.spacedot.acubesat.file_handling.utils.PacketParser;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import gr.spacedot.acubesat.file_handling.enums.PacketType;
 
 import static gr.spacedot.acubesat.file_handling.utils.PacketParser.DELIMITER;
@@ -80,7 +90,72 @@ public class TMParser {
 
 
             packetOffset += length;
+            String bucketName = "testingBucket";
+            if(!bucketExists(bucketName)){
+                createBucket(bucketName);   
+            }
+            uploadObjectToBucket(fileSegment, bucketName);
 
+        }
+    }
+
+    public boolean bucketExists(String bucketName){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest getBucket = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8090/api/buckets/AcubeSAT/" + bucketName))
+            .timeout(Duration.ofSeconds(10))
+            .GET()
+            .build();
+
+        try{
+            HttpResponse<String> response = client.send(getBucket, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() != HttpResponseStatus.OK.code()){
+                LOGGER.info(response.body());
+                return false;
+            }
+        }catch(Exception e){
+            LOGGER.info("Error sending request" + e);
+        }
+        return true;
+    }
+
+    public void createBucket(String bucketName){
+        JsonObject mainBody = new JsonObject();
+        HttpClient client = HttpClient.newHttpClient();
+        mainBody.addProperty("name", bucketName);
+
+        HttpRequest createBucketRequest = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8090/api/buckets/AcubeSAT"))
+            .POST(HttpRequest.BodyPublishers.ofString(mainBody.toString()))
+            .build();
+
+        try{
+            HttpResponse<String> response = client.send(createBucketRequest, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() != HttpResponseStatus.OK.code())
+                LOGGER.info(response.body());
+        }catch(Exception e){
+            LOGGER.info("Error sending request" + e);
+        }
+    }
+
+    public void uploadObjectToBucket(byte[] object, String bucketName){
+        String base64String = Base64.getEncoder().encodeToString(object);
+        HttpClient client = HttpClient.newHttpClient();
+        JsonObject mainBody = new JsonObject();
+        mainBody.addProperty("data", base64String);
+
+        HttpRequest uploadObjectRequest = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8090/api/buckets/AcubeSAT/" + bucketName + "/objects/a3sat.png"))
+            .header("Content-Type", "text/html")
+            .POST(HttpRequest.BodyPublishers.ofString(mainBody.toString()))
+            .build();
+
+        try{
+            HttpResponse<String> response = client.send(uploadObjectRequest, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() != HttpResponseStatus.OK.code())
+                LOGGER.info(response.body());
+        }catch(Exception e){
+            LOGGER.info("Error sending request" + e);
         }
     }
 }
