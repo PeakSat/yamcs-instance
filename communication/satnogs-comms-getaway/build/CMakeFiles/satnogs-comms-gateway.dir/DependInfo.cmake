571620c7a12a15f5295c4f63e93269e361d61cb0
George Kikas <kikasgeorg@ece.auth.gr>
2025-05-19 15:07:02 +0300

    Fix GNURadio gateway config

---
+++ communication/satnogs-comms-getaway/build/CMakeFiles/satnogs-comms-gateway.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kikas/repos/yamcs-instance/communication/satnogs-comms-getaway/src/can_transport.cpp" "CMakeFiles/satnogs-comms-gateway.dir/src/can_transport.cpp.o" "gcc" "CMakeFiles/satnogs-comms-gateway.dir/src/can_transport.cpp.o.d"
+  "/home/kikas/repos/yamcs-instance/communication/satnogs-comms-getaway/src/gr_zmq_transport.cpp" "CMakeFiles/satnogs-comms-gateway.dir/src/gr_zmq_transport.cpp.o" "gcc" "CMakeFiles/satnogs-comms-gateway.dir/src/gr_zmq_transport.cpp.o.d"
+  "/home/kikas/repos/yamcs-instance/communication/satnogs-comms-getaway/src/main.cpp" "CMakeFiles/satnogs-comms-gateway.dir/src/main.cpp.o" "gcc" "CMakeFiles/satnogs-comms-gateway.dir/src/main.cpp.o.d"
+  "/home/kikas/repos/yamcs-instance/communication/satnogs-comms-getaway/src/udp_transport.cpp" "CMakeFiles/satnogs-comms-gateway.dir/src/udp_transport.cpp.o" "gcc" "CMakeFiles/satnogs-comms-gateway.dir/src/udp_transport.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ communication/satnogs-comms-getaway/build/CMakeFiles/satnogs-comms-gateway.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/satnogs-comms-gateway.dir/src/can_transport.cpp.o"
+  "CMakeFiles/satnogs-comms-gateway.dir/src/can_transport.cpp.o.d"
+  "CMakeFiles/satnogs-comms-gateway.dir/src/gr_zmq_transport.cpp.o"
+  "CMakeFiles/satnogs-comms-gateway.dir/src/gr_zmq_transport.cpp.o.d"
+  "CMakeFiles/satnogs-comms-gateway.dir/src/main.cpp.o"
+  "CMakeFiles/satnogs-comms-gateway.dir/src/main.cpp.o.d"
+  "CMakeFiles/satnogs-comms-gateway.dir/src/udp_transport.cpp.o"
+  "CMakeFiles/satnogs-comms-gateway.dir/src/udp_transport.cpp.o.d"
+  "satnogs-comms-gateway"
+  "satnogs-comms-gateway.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/satnogs-comms-gateway.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
